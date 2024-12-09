a4464a42340349cf0f0457bc56716878e778d377
hacker4world <zonegaming514@gmail.com>
2024-12-09 09:57:59 +0100

    reset password and modify profile informations

--- routers/authentification.js
+++ routers/authentification.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import {
   AddEmployee,
-  login,
-  SendCodeResetPassword,
-  verificatioCode,
+  login, resetPassword,
+  sendPasswordResetCode,
+  verifyResetCode,
 } from "../controllers/employee.js";
 
 export const router = Router();
 
 router.post("/addEmploye", AddEmployee);
 router.post("/login", login);
-router.post("/send-reset-email", SendCodeResetPassword);
-router.post("/resetCode", verificatioCode);
+router.post("/send-reset-email", sendPasswordResetCode);
+router.post("/verify-reset-code", verifyResetCode);
+router.post("/reset-code", resetPassword);
